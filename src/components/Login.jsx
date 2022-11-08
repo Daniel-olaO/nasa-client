d2a4f7fa854488e38941fd75a4ba29c7d1add586
Daniel-olaO <danieladedeji135@gmail.com>
2022-11-08 02:59:13 -0500

    Added form authencation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,64 @@
 import {React, useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import Container from '@mui/material/Container';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Input from '@mui/material/Input';
 import Button from '@mui/material/Button';
 import {FormGroup} from '@mui/material';
+import Cookies from 'universal-cookie';
 import Navbar from './Navbar';
 import '../App.css';
 
-const Login = () => {
+
+function login(user) {
+  const baseUrl = process.env.API_BASE_URL || 'http://localhost:8000';
+  return fetch(`${baseUrl}/api/login`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(user),
+  })
+      .then((data) => data.json());
+}
+
+const Login = ({setIsLoggedIn}) => {
+  const navigate = useNavigate();
+  const cookie = new Cookies();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [showMessage, setShowMessage] = useState(false);
 
-  const handleSubmit = () => {
-    alert('Email: ' + email + ' Password: ' + password);
+
+  const handleSubmit = async () => {
+    const response = await login({email, password});
+    console.log(response);
+    if (response.token) {
+      const duration = new Date();
+      duration.setTime(duration.getTime() + (1 * 60 * 60 * 1000));
+      cookies.set('token', response.token, {path: '/', expires: duration});
+      dataSet = {
+        'id': response.user.id,
+        'name': response.user.name,
+        'isSubscribed': response.user.isSubscribed,
+      };
+      localStorage.setItem('data', JSON.stringify(dataSet));
+      setIsLoggedIn(true);
+      navigate('/home');
+    } else {
+      console.log(response);
+      setMessage(response.detail);
+      setShowMessage(true);
+      setTimeout(() => {
+        setShowMessage(false);
+      }, 2000);
+    }
+    setEmail('');
+    setPassword('');
   };
+
   return (
     <>
       <Navbar/>
