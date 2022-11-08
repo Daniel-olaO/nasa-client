274b074ada77314d11723d12becc3907f8a49e7b
Daniel-olaO <danieladedeji135@gmail.com>
2022-11-08 03:32:18 -0500

    Added authentication

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import {Routes, Route} from 'react-router-dom';
+import Cookies from 'universal-cookie';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import NotFound from './components/NotFound';
 import Dashboard from './components/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const cookies = new Cookies();
+
+  useEffect(() => {
+    if (cookies.get('token')) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Dashboard />} />
+        <Route path="/home" element={
+          <ProtectedRoute isAuth={isAuthenticated}>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,39 @@
-import {React, useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import Container from '@mui/material/Container';
 import '../App.css';
 
-const Dashboard = () => {
+function logOut() {
+  const baseUrl = process.env.API_BASE_URL ||'http://localhost:8000';
+  return fetch(baseUrl + '/api/logout', {
+    method: 'POST',
+  })
+      .then((response) => {
+        return response.json();
+      });
+}
+function toggleSubscription() {
+  const id = localStorage.getItem('data').id;
+  const baseUrl = process.env.API_BASE_URL ||'http://localhost:8000';
+  return fetch(baseUrl + '/api/toggle-subscription/' + id, {
+    method: 'POST',
+  })
+      .then((response) => {
+        return response.json();
+      });
+}
+
+const Dashboard = ({setIsAuthenticated}) => {
   const [isSubscribed, setIsSubscribed] = useState(true);
+
+  const logOut = async () => {
+    const cookies = new Cookies();
+    localStorage.removeItem('data');
+    cookies.remove('token');
+    setIsAuthenticated(false);
+  };
+  useEffect(() => {
+    setIsSubscribed(localStorage.getItem('data').isSubscribed);
+  }, []);
   return (
     <>
       <Container maxWidth="sm">
