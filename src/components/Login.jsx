471eee40351ce685c7da318b5ee97d52e5bfb3fe
daniel <danieladedeji997@yahoo.com>
2023-03-14 16:41:33 -0400

    Used formik boilerplate

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,11 +4,12 @@ import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {FormGroup, Alert} from '@mui/material';
-import {useFormik} from 'formik';
+import {useFormik, Formik, Form, Field, ErrorMessage} from 'formik';
 import * as Yup from 'yup';
 import Cookies from 'universal-cookie';
 import Navbar from './Navbar';
 import '../App.css';
+import {async} from 'q';
 
 
 function login(user) {
@@ -27,10 +28,12 @@ function login(user) {
 const validationSchema = Yup.object({
   email: Yup
       .string()
+      .default('')
       .email('Invalid email format')
       .required('Email is required'),
   password: Yup
       .string()
+      .default('')
       .required('Password is required'),
 });
 
@@ -41,38 +44,30 @@ const Login = ({setIsAuthenticated}) => {
   const [showMessage, setShowMessage] = useState(false);
   const [message, setMessage] = useState('');
 
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    validationSchema: validationSchema,
-    onSubmit: async (values) => {
-      setLoading(true);
-      const response = await login(values);
-      setLoading(false);
-      console.log(response);
-      if (response.jwt) {
-        const duration = new Date();
-        duration.setTime(duration.getTime() + (1 * 60 * 60 * 1000));
-        cookies.set('token', response.jwt, {path: '/', expires: duration});
-        const dataSet = {
-          'id': response.user.id,
-          'name': response.user.name,
-          'isSubscribed': response.user.isSubscribed,
-        };
-        localStorage.setItem('data', JSON.stringify(dataSet));
-        setIsAuthenticated(true);
-        navigate('/home');
-      } else if (response.detail) {
-        setMessage(response.detail);
-        setShowMessage(true);
-        setTimeout(() => {
-          setShowMessage(false);
-        }, 2000);
-      }
-    },
-  });
+  const handleLogin = async (values) => {
+    setLoading(true);
+    const response = await login(values);
+    setLoading(false);
+    if (response.jwt) {
+      const duration = new Date();
+      duration.setTime(duration.getTime() + (1 * 60 * 60 * 1000));
+      cookies.set('token', response.jwt, {path: '/', expires: duration});
+      const dataSet = {
+        'id': response.user.id,
+        'name': response.user.name,
+        'isSubscribed': response.user.isSubscribed,
+      };
+      localStorage.setItem('data', JSON.stringify(dataSet));
+      setIsAuthenticated(true);
+      navigate('/home');
+    } else if (response.detail) {
+      setMessage(response.detail);
+      setShowMessage(true);
+      setTimeout(() => {
+        setShowMessage(false);
+      }, 2000);
+    }
+  };
 
   return (
     <>
@@ -81,37 +76,35 @@ const Login = ({setIsAuthenticated}) => {
         {loading && <Alert severity="info">Loading...</Alert>}
         {showMessage && <Alert severity="error">{message}</Alert>}
         <h2>Login</h2>
-        <form onSubmit={formik.handleSubmit}>
-          <div className='form-group'>
-            <TextField id='filled-basic email'
-              variant='filled'
-              label='Email'
-              name='email'
-              value={formik.values.email}
-              placeholder="ryan.doe@example.com"
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-            />
-          </div>
-          <div className='form-group'>
-            <TextField id='filled-basic password'
-              variant='filled'
-              label='Password'
-              name='password'
-              type="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-              helperText={formik.touched.password && formik.errors.password}
-            />
-          </div>
-          <div className="form-group">
-            <Button variant="contained"
-              type='submit'
-            >Login</Button>
-          </div>
-        </form>
+        <Formik
+          initialValues={validationSchema.default()}
+          validationSchema={validationSchema}
+          onSubmit={async (values) => {
+            await handleLogin(values);
+          }}
+        >
+          {(props)=>(
+            <Form>
+              <div className="form-group">
+                <Field name="email" type="email"
+                  as={TextField} label="Email" variant="filled"
+                />
+                <ErrorMessage name="email" component="div" className="error"/>
+              </div>
+              <div className="form-group">
+                <Field name="password" type="password"
+                  as={TextField} label="Password"
+                  variant="filled"/>
+                <ErrorMessage name="password" component="div"className="error"/>
+              </div>
+              <div className="form-group">
+                <Button type="submit" variant="contained" color="primary">
+                Login
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
 
         <h5>Don't have an account? <Link to='/signup'>Sign up</Link></h5>
       </Container>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -4,11 +4,12 @@ import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {FormGroup, Alert} from '@mui/material';
-import {useFormik} from 'formik';
+import {useFormik, Formik, Form, Field, ErrorMessage} from 'formik';
 import * as Yup from 'yup';
 import Cookies from 'universal-cookie';
 import '../App.css';
 import Navbar from './Navbar';
+import {async} from 'q';
 
 
 function signUp(user) {
@@ -36,17 +37,21 @@ function login(user) {
 const validationSchema = Yup.object({
   name: Yup
       .string()
+      .default('')
       .required('Name is required'),
   phone: Yup
       .string()
+      .default('')
       .required('Phone is required')
       .matches(/^\+[0-9]{9,15}$/, 'Invalid phone number: +123456789'),
   email: Yup
       .string()
+      .default('')
       .email('Invalid email format')
       .required('Email is required'),
   password: Yup
       .string()
+      .default('')
       .required('Password is required'),
 });
 
@@ -58,41 +63,32 @@ const Signup = ({setIsAuthenticated}) => {
   const [showMessage, setShowMessage] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      phone: '',
-      email: '',
-      password: '',
-    },
-    validationSchema: validationSchema,
-    onSubmit: async (values) => {
-      setLoading(true);
-      const response = await signUp(values);
-      setLoading(false);
-      if (response.id) {
-        const loginResponse = await login({
-          email: values.email,
-          password: values.password,
-        });
-        if (loginResponse.jwt) {
-          const duration = new Date();
-          duration.setTime(duration.getTime() + (1 * 60 * 60 * 1000));
-          cookies.set('token',
-              loginResponse.jwt, {path: '/', expires: duration});
-          setIsAuthenticated(true);
-          navigate('/home');
-        }
-      } else {
-        setMessage(response.phone[0] || response.email[0] ||
-          'error: please try again');
-        setShowMessage(true);
-        setTimeout(() => {
-          setShowMessage(false);
-        }, 2000);
+  const handleSignUp = async (values) => {
+    setLoading(true);
+    const response = await signUp(values);
+    setLoading(false);
+    if (response.id) {
+      const loginResponse = await login({
+        email: values.email,
+        password: values.password,
+      });
+      if (loginResponse.jwt) {
+        const duration = new Date();
+        duration.setTime(duration.getTime() + (1 * 60 * 60 * 1000));
+        cookies.set('token',
+            loginResponse.jwt, {path: '/', expires: duration});
+        setIsAuthenticated(true);
+        navigate('/home');
       }
-    },
-  });
+    } else {
+      setMessage(response.phone[0] || response.email[0] ||
+          'error: please try again');
+      setShowMessage(true);
+      setTimeout(() => {
+        setShowMessage(false);
+      }, 2000);
+    }
+  };
 
   return (
     <>
@@ -101,63 +97,42 @@ const Signup = ({setIsAuthenticated}) => {
         {loading && <Alert severity="info">Loading...</Alert>}
         {showMessage && <Alert severity="error">{message}</Alert>}
         <h2>Sign Up</h2>
-        <form onSubmit={formik.handleSubmit}>
-          <div className='form-group'>
-            <TextField id='filled-basic name'
-              variant='filled'
-              label='Name'
-              name='name'
-              placeholder='Ryan Doe'
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              error={formik.touched.name && Boolean(formik.errors.name)}
-              helperText={formik.touched.name && formik.errors.name}
-            />
-          </div>
-          <div className='form-group'>
-            <TextField id='filled-basic email'
-              variant='filled'
-              label='Email'
-              type='email'
-              name='email'
-              placeholder='ryan.doe@example.com'
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-            />
-          </div>
-          <div className='form-group'>
-            <TextField id='filled-basic phone'
-              variant='filled'
-              label="Phone"
-              name="phone"
-              type='text'
-              placeholder="+123456789"
-              value={formik.values.phone}
-              onChange={formik.handleChange}
-              error={formik.touched.phone && Boolean(formik.errors.phone)}
-              helperText={formik.touched.phone && formik.errors.phone}
-            />
-          </div>
-          <div className='form-group'>
-            <TextField id='filled-basic password'
-              variant='filled'
-              label='Password'
-              name='password'
-              type="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-              helperText={formik.touched.password && formik.errors.password}
-            />
-          </div>
-          <div className='form-group'>
-            <Button variant="contained"
-              type='submit'
-            >Sign Up</Button>
-          </div>
-        </form>
+        <Formik
+          initialValues={validationSchema.default()}
+          validationSchema={validationSchema}
+          onSubmit={async (values) => {
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          {(props)=>(
+            <Form>
+              <div className="form-group">
+                <Field type="text" name="name" placeholder="Ryan Doe"
+                  as={TextField} label="Full Name" variant="filled"/>
+                <ErrorMessage name="name" component="div" className="error"/>
+              </div>
+              <div className="form-group">
+                <Field type="text" name="email" placeholder="ryan.doe@abc.com"
+                  as={TextField} label="Email" variant="filled"/>
+                <ErrorMessage name="email" component="div" className="error"/>
+              </div>
+              <div className="form-group">
+                <Field type="text" name="phone" placeholder="+123456789"
+                  as={TextField} label="Phone" variant="filled"/>
+                <ErrorMessage name="phone" component="div" className="error"/>
+              </div>
+              <div className="form-group">
+                <Field type="password" name="password" placeholder="********"
+                  as={TextField} label="Password" variant="filled"/>
+                <ErrorMessage
+                  name="password" component="div" className="error"/>
+              </div>
+              <Button type="submit" variant="contained" color="primary">
+                Sign Up
+              </Button>
+            </Form>
+          )}
+        </Formik>
       </Container>
       <h5>Already have an account? <Link to="/">Login</Link></h5>
     </>
